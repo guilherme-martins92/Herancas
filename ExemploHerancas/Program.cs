e6f7ddd8fced4e0e4f88ebbca21c7b448efbcf9a
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2021-04-13 19:09:01 -0300

    Aula sobre heranças

---
+++ ExemploHerancas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ExemploHerancas.Entities;
+
+namespace ExemploHerancas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "ALex", 500);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500, 0.01);
+
+            acc1.Withdraw(10);
+            acc2.Withdraw(10);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
